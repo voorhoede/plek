dd52735708f60363867ca97cd2a4248d73b741c2
Selwyn <talk@selwyn.cc>
2018-07-09 22:55:57 +0200

    Show CLI help message when appropriate regardless of the environment
    Closes #3

--- packages/cli/src/main.js
+++ packages/cli/src/main.js
@@ -24,7 +24,7 @@ const backendAxios = axios.create({
   }),
 });
 
-const cleanupFlow = ciEnv => command => {
+const cleanupFlow = command => ciEnv => {
   backendAxios({
     data: {
       ciEnv,
@@ -38,7 +38,7 @@ const cleanupFlow = ciEnv => command => {
   return command();
 };
 
-const deployFlow = ciEnv => command => {
+const deployFlow = command => ciEnv => {
   backendAxios({
     data: {
       ciEnv,
@@ -66,7 +66,7 @@ const deployFlow = ciEnv => command => {
   });
 };
 
-const aliasFlow = ciEnv => (command, domain) => {
+const aliasFlow = (command, domain) => ciEnv => {
   const domainDivider = hasSubdomain(domain) ? '-' : '.';
 
   backendAxios({
@@ -109,45 +109,43 @@ const logFatalError = error => {
   process.exit(1);
 };
 
-getCiEnv().then(ciEnv => {
-  const cleanup = cleanupFlow(ciEnv);
-  const deploy = deployFlow(ciEnv);
-  const alias = aliasFlow(ciEnv);
+const [cleanup, deploy, alias] = [cleanupFlow, deployFlow, aliasFlow].map(
+  flow => (...args) => getCiEnv().then(flow(...args))
+);
 
-  commander.command('cleanup <command>').action(command => {
-    cleanup(() => exec(command));
-  });
-
-  commander.command('deploy <command>').action(command => {
-    deploy(() => exec(command).then(getStdout));
-  });
-
-  commander.command('alias <command> <domain>').action((command, domain) => {
-    alias(() => exec(command), domain);
-  });
-
-  commander
-    .command('now <domain>')
-    .option('-a, --app <app>', 'Zeit Now app name')
-    .option('-c, --config <config>', 'Zeit Now CLI configuration flags', '')
-    .option('--team [team]', 'Zeit team name', team => ({
-      flag: `--team ${team}`,
-      slug: team,
-    }))
-    .action((domain, { config, app, team }) => {
-      if (!app) throw Error('Missing Zeit Now app name argument');
-      if (!process.env.NOW_TOKEN)
-        throw new Error('Missing NOW_TOKEN environment variable.');
-
-      cleanup(now.cleanup({ app, teamSlug: team.slug })).then(
-        deploy(now.deploy({ config, app, teamFlag: team.flag })).then(url =>
-          alias(now.alias({ url, teamFlag: team.flag }), domain)
-        )
-      );
-    });
-
-  commander.parse(process.argv);
+commander.command('cleanup <command>').action(command => {
+  cleanup(() => exec(command));
 });
+
+commander.command('deploy <command>').action(command => {
+  deploy(() => exec(command).then(getStdout));
+});
+
+commander.command('alias <command> <domain>').action((command, domain) => {
+  alias(() => exec(command), domain);
+});
+
+commander
+  .command('now <domain>')
+  .option('-a, --app <app>', 'Zeit Now app name')
+  .option('-c, --config <config>', 'Zeit Now CLI configuration flags', '')
+  .option('--team [team]', 'Zeit team name', team => ({
+    flag: `--team ${team}`,
+    slug: team,
+  }))
+  .action((domain, { config, app, team }) => {
+    if (!app) throw Error('Missing Zeit Now app name argument');
+    if (!process.env.NOW_TOKEN)
+      throw new Error('Missing NOW_TOKEN environment variable.');
+
+    cleanup(now.cleanup({ app, teamSlug: team.slug })).then(
+      deploy(now.deploy({ config, app, teamFlag: team.flag })).then(url =>
+        alias(now.alias({ url, teamFlag: team.flag }), domain)
+      )
+    );
+  });
+
+commander.parse(process.argv);
 
 process.title = 'plek';
 process.on('unhandledRejection', logFatalError);
