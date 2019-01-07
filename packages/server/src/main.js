0919f362cc3c5fb0ed2aa21fce2b51000e560f02
Selwyn <talk@selwyn.cc>
2019-01-07 11:19:46 +0100

    Upgrade Plek server to use Now Platform 2.0
    Introduces a serverless lambda context so new configuration and relative 
    file paths are needed.

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const dotenv = require('dotenv-safe');
+const path = require('path');
 const raven = require('raven');
 const server = require('./server.js');
 
-dotenv.config();
+dotenv.config({ example: path.join(__dirname, '..', '.env.example') });
 
 raven
   .config('https://032b525989a645b991503e629465f8c4@sentry.io/1260219')
