f7804bdaa0ee0ac81cdb9746938119bc6cb01a82
Selwyn <talk@selwyn.cc>
2019-01-14 13:52:11 +0100

    Revert "Upgrade Plek server to use Now Platform 2.0"
    This reverts commit 0919f362cc3c5fb0ed2aa21fce2b51000e560f02.

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,11 +1,10 @@
 'use strict';
 
 const dotenv = require('dotenv-safe');
-const path = require('path');
 const raven = require('raven');
 const server = require('./server.js');
 
-dotenv.config({ example: path.join(__dirname, '..', '.env.example') });
+dotenv.config();
 
 raven
   .config('https://032b525989a645b991503e629465f8c4@sentry.io/1260219')
